5117b2c7f1d13c4116d535ae321253cd011ec5bb
Alex Ptakhin <alex.ptakhin@prestatech.com>
2024-12-01 10:17:23 +0100

    Day one - part one

---
+++ src/day1/one.clj
@@ -0,0 +1,28 @@
+(ns day1.one)
+
+(comment
+  "Place input to the `input` file in the same directory as this file"
+  :rcf)
+
+(defn parse-file [file-path]
+      (with-open [rdr (clojure.java.io/reader file-path)]
+            (doall
+                  (map #(mapv read-string (clojure.string/split % #"\s+"))
+                               (line-seq rdr)))))
+
+
+(def data (parse-file "src/1/input"))
+(comment
+  (second (first data))
+  :rcf)
+
+(def first-list (map first data))
+(def second-list (map second data))
+
+(def sorted-first-list (sort first-list))
+(def sorted-second-list (sort second-list))
+
+(def differences (map abs(map - sorted-first-list sorted-second-list)))
+(apply + differences)
+(def sum (apply + differences))
+(println sum)
